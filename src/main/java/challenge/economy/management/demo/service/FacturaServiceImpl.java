880adfec443af32039b02dd1abb125f213d6ff46
Pedro Serra <pedroserra86@gmail.com>
2023-03-27 01:47:50 -0300

    cuarto commit: Functional

--- src/main/java/challenge/economy/management/demo/service/FacturaServiceImpl.java
+++ src/main/java/challenge/economy/management/demo/service/FacturaServiceImpl.java
@@ -6,14 +6,11 @@ import challenge.economy.management.demo.domain.InformeResponse;
 import challenge.economy.management.demo.model.entity.Factura;
 import challenge.economy.management.demo.model.repository.FacturaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class FacturaServiceImpl implements IFacturaService {
@@ -26,7 +23,7 @@ public class FacturaServiceImpl implements IFacturaService {
         if (factura.getFechaPago() != null) {
             factura.setPagado(true);
         }
-    facturaRepo.save(factura);
+        facturaRepo.save(factura);
     }
 
     @Override
@@ -56,53 +53,64 @@ public class FacturaServiceImpl implements IFacturaService {
         InformeResponse informeResponse = new InformeResponse();
         Map<Month, Double> hashMeses = new HashMap<>();
 
+        //Functional
 
+        List<Factura> facturas = (List<Factura>) facturaRepo.findAll();
 
-        for (Factura factura : facturaRepo.findAll()){
+        List<Factura> facturasEnRango = seleccionarFacturasEnRango(facturas, solicitudInformePeriodo);
+
+        Map<Object, Double> hashMeses2 = facturasEnRango.stream()
+                .collect(Collectors.groupingBy(factura -> factura.getFechaPago().getMonth(),
+                        Collectors.summingDouble(Factura::getDeuda)));
+        //gasto total func
+        Double gastoTotalF = facturasEnRango.stream()
+                .map(factura -> factura.getDeuda())
+                .reduce(0.0, Double::sum);
+
+        //Informe total func
+        List<InformeMensual> informeTotal = new ArrayList<>();
+        hashMeses2.forEach((month, aDouble) -> informeTotal.add(new InformeMensual((Month) month, aDouble)));
+
+        //Gasto maximo
+        Double gastoMaximoF = informeTotal.stream()
+                .map(informeMensual -> informeMensual.getGastoTotal())
+                .reduce(Double.MIN_VALUE, (x, y) -> x > y ? x : y);
+        //Mes Gasto Maximo
+        Month mesGastoMaximoF = getListaInformesOrdenada(informeTotal).get(getListaInformesOrdenada(informeTotal).size() - 1).getMes();
+
+        //Mes gasto minimo
+        Month mesGastoMinimoF = getListaInformesOrdenada(informeTotal).get(0).getMes();
+
+        //Gasto minimo
+        Double gastoMinimoF = informeTotal.stream()
+                .map(informeMensual -> informeMensual.getGastoTotal())
+                .reduce(Double.MAX_VALUE, (x, y) -> x < y ? x : y);
+
+        for (Factura factura : facturaRepo.findAll()) {
 
             if ((factura.getFechaPago() != null) && factura.getFechaPago().isAfter(solicitudInformePeriodo.getFechaInicio()) &&
-                    factura.getFechaPago().isBefore(solicitudInformePeriodo.getFechaFinal())){
-
+                    factura.getFechaPago().isBefore(solicitudInformePeriodo.getFechaFinal())) {
+                List<Factura> facturaPedro = new ArrayList<>();
                 //HashMap de los meses
                 Double valorDeFactura;
-                if (hashMeses.containsKey(factura.getFechaPago().getMonth())){
+                if (hashMeses.containsKey(factura.getFechaPago().getMonth())) {
                     valorDeFactura = hashMeses.get(factura.getFechaPago().getMonth()) + factura.getDeuda();
                     hashMeses.put(factura.getFechaPago().getMonth(), valorDeFactura);
                 } else hashMeses.put(factura.getFechaPago().getMonth(), factura.getDeuda());
 
-                //Gasto Maximo
-/*
-                if (factura.getDeuda() > gastoMaximo){
-                    gastoMaximo = factura.getDeuda();
-                    mesGastoMaximo = factura.getFechaPago().getMonth();
-                }
-
-                //Gasto Minimo
-                if (factura.getDeuda() < gastoMinimo){
-                    gastoMinimo = factura.getDeuda();
-                    mesGastoMinimo = factura.getFechaPago().getMonth();
-                }
-*/
                 //Gasto total
                 gastoTotal += factura.getDeuda();
-
-
-
             }
         }
 
-
-
-
-
-        //Mes gasto maximo
-        List<InformeMensual> informe = new ArrayList<>(hashMeses.size());
-        hashMeses.forEach((month, aDouble) -> informe.add(new InformeMensual(month, aDouble)));
+        //Informe mensual
+//        List<InformeMensual> informeTotal = new ArrayList<>(hashMeses.size());
+//        hashMeses.forEach((month, aDouble) -> informeTotal.add(new InformeMensual(month, aDouble)));
 
         //mes gasto maximo
         Double variable = 0.0;
-        for (InformeMensual inf : informe){
-            if (inf.getGastoTotal()>variable){
+        for (InformeMensual inf : informeTotal) {
+            if (inf.getGastoTotal() > variable) {
                 variable = inf.getGastoTotal();
                 mesGastoMaximo = inf.getMes();
             }
@@ -110,8 +118,8 @@ public class FacturaServiceImpl implements IFacturaService {
 
         //mes gasto minimo
         Double variable2 = 999999.0;
-        for (InformeMensual inf : informe){
-            if (inf.getGastoTotal()<variable2){
+        for (InformeMensual inf : informeTotal) {
+            if (inf.getGastoTotal() < variable2) {
                 variable2 = inf.getGastoTotal();
                 mesGastoMinimo = inf.getMes();
             }
@@ -124,12 +132,12 @@ public class FacturaServiceImpl implements IFacturaService {
         gastoMinimo = hashMeses.get(mesGastoMinimo);
 
 
-        informeResponse.setGastoMinimo(gastoMinimo);
-        informeResponse.setMesGastoMinimo(mesGastoMinimo);
-        informeResponse.setGastoMaximo(gastoMaximo);
-        informeResponse.setMesGastoMaximo(mesGastoMaximo);
-        informeResponse.setGastoTotal(gastoTotal);
-        informeResponse.setReporteMensual(informe);
+        informeResponse.setGastoMinimo(gastoMinimoF);
+        informeResponse.setMesGastoMinimo(mesGastoMinimoF);
+        informeResponse.setGastoMaximo(gastoMaximoF);
+        informeResponse.setMesGastoMaximo(mesGastoMaximoF);
+        informeResponse.setGastoTotal(gastoTotalF);
+        informeResponse.setReporteMensual(informeTotal);
 
         //event
 
@@ -143,6 +151,18 @@ public class FacturaServiceImpl implements IFacturaService {
     }
 
 
+    private static List<Factura> seleccionarFacturasEnRango(List<Factura> facturas, InformeRequest solicitudInformePeriodo) {
+        return facturas.stream()
+                .filter(factura -> factura.getFechaPago() != null)
+                .filter(factura -> factura.getFechaPago().isAfter(solicitudInformePeriodo.getFechaInicio()))
+                .filter(factura -> factura.getFechaPago().isBefore(solicitudInformePeriodo.getFechaFinal()))
+                .collect(Collectors.toList());
+    }
 
+    private static List<InformeMensual> getListaInformesOrdenada(List<InformeMensual> informeTotal) {
+        return informeTotal.stream()
+                .sorted(Comparator.comparingDouble(InformeMensual::getGastoTotal))
+                .collect(Collectors.toList());
 
+    }
 }
